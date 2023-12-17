d3c0ab734f3d44abb7199b1f107cf7715211246c
Himanshu <himanshu16315@gmail.com>
2023-12-17 15:11:24 -0500

    Navbar created 🍀

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import logo from "../assets/Asset 16@4x.png";
 
-function Nav() {
-  return <div className="nav">Nav</div>;
-}
+const Nav = () => {
+  const links = [
+    "Home",
+    "About",
+    "Menu",
+    "Reservations",
+    "Order Online",
+    "Login",
+  ];
+  return (
+    <nav>
+      <div className="nav_logo">
+        <a href="/">
+          <img src={logo} alt="" className="logo_image" />
+        </a>
+      </div>
+      <div className="nav_links">
+        <ul>
+          {links.map((link) => (
+            <li>
+              <a href="/">{link}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
